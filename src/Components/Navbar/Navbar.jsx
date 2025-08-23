61797f4901411e5f6ca371e838d48cbcc67fde04
bw852023F <bw85@vt.edu>
2025-08-23 18:48:33 -0400

    Remove navbar_logo_jpeg

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import navbar_logo from '../../assets/navbar-removebg.png'
-import navbar_logo_jpeg from '../../assets/navbar-removebg.jpeg'
 import menu_icon from '../../assets/menu-icon.png'
 // import { Link as ScrollLink } from 'react-scroll';
 import { useNavigate, useLocation } from 'react-router-dom'
