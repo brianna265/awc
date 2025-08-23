125ddfff99dbcce32b972c1ed436da49886ee561
bw852023F <bw85@vt.edu>
2025-08-23 12:52:34 -0400

    Copy navbar logo to public folder, change reference

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -130,7 +130,7 @@ const Navbar = () => {
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       {/* navbar logo */}
       <button className='nav-btn' onClick={() => handleNavClick('home')}>
-        <img src={navbar_logo} alt="navbar-logo" className='navbar-logo'/>
+        <img src='/awc/images/AWCLogo-removebg.png' alt="navbar-logo" className='navbar-logo'/>
       </button>
       
       <ul className={mobileMenu ? '':'hide-mobile-menu'}>
