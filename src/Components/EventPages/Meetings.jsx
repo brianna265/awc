90e105029245a11a1ef4c35803d483558059cc2a
bytwang <bw85@vt.edu>
2025-09-27 22:50:10 -0400

    Update rsvp link

--- src/Components/EventPages/Meetings.jsx
+++ src/Components/EventPages/Meetings.jsx
@@ -53,7 +53,7 @@ const Meetings = () => {
       </div>
       <div className='rsvp-section'>
           <button className='btn-primary'>
-            <a href="https://forms.gle/VcQvCUXHkKwioVKj6" target="_blank" rel='noopener noreferrer'>
+            <a href="https://forms.gle/5gs47jitntFWkKe17" target="_blank" rel='noopener noreferrer'>
               RSVP to our next meeting!
             </a>
           </button>
